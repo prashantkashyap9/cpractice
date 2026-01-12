d72886a0c2527bb20485e079ef5b78b2bde0ce5f
Prshant kashyap <prashantkashyap9997@gmail.com>
2026-01-12 13:44:57 +0530

    To check a person can vote or not

---
+++ vote.c
@@ -0,0 +1,14 @@
+// to check a person is eligible for vote or not
+#include<stdio.h>
+int main(){
+    int age;
+    printf("enter your age: ");
+    scanf("%d",&age);
+    if(age>=18){
+        printf("you are eligible for vote");
+    }
+    else{
+        printf("you are not eligible for vote");
+    }
+    return 0;
+}
