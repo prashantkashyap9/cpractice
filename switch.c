ecada83a6964ceb20db8304ac5ff336ddf978557
Prshant kashyap <prashantkashyap9997@gmail.com>
2026-01-12 15:32:38 +0530

    Switch statement commit

---
+++ switch.c
@@ -0,0 +1,35 @@
+//Write a program to print the day of the week using switch case.
+#include<stdio.h>
+int main(){
+    int day;
+    printf("Enter a number (1-7): ");
+    scanf("%d",&day);
+    switch(day){
+        case 1:
+        printf("monday");
+        break;
+        case 2:
+        printf("tuesday");
+        break;
+        case 3:
+        printf("wednesday");
+        break;
+        case 4:
+        printf("thursday"); 
+        break;
+        case 5:
+        printf("friday");
+
+        break;
+        case 6:
+        printf("saturday");
+        break;
+        case 7:
+        printf("sunday");
+        break;
+        default:
+        printf("invalid input");
+
+    }
+    return 0;
+}
