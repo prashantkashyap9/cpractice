bac8adcef8e2d528da0b8fb06607d42498b3eea6
Prshant kashyap <prashantkashyap9997@gmail.com>
2026-01-12 13:34:36 +0530

    ifelse file commit

---
+++ ifelse.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a=10,b=20,c=30;
+    if((a+b)==c){
+        printf("The sum of a and b is equal to c\n");
+    }
+    else{
+        printf("The sum of a and b is not equal to c\n");
+
+    }
+    return 0;
+}
