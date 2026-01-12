85298c44b2a43a872fa823c2c1d16e22651eeba7
Prshant kashyap <prashantkashyap9997@gmail.com>
2026-01-12 18:19:29 +0530

    print * pattern using nested loop

---
+++ nestedloop.c
@@ -0,0 +1,10 @@
+//print a * pattern using nested loop
+#include<stdio.h>
+int main(){
+    for(int i=1; i<=5; i++){
+        for(int j=1; j<=i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
